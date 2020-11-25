f29eef27ebb57175c8d759687a654725e944c6eb
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-11-25 16:20:03 -0700

    articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new(params[:article])
+
+    @article.save
+    redirect_to @article
+  end
+
+  def create
+    render plain: params[:article].inspect
   end
 end
